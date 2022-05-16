5e476439bf8ded14ef1dacb0228eea27e1e27d67
Abbas K. Radam <abbasabbe1995@gmail.com>
2022-05-16 16:32:38 +0300

    add AspnetRunBasics

--- src/piGateways/Shopping.Aggregator/Program.cs
+++ src/piGateways/Shopping.Aggregator/Program.cs
@@ -1,6 +1,9 @@
+using Shopping.Aggregator;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHttpClientServices(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ src/piGateways/Shopping.Aggregator/ServiceRegistration.cs
@@ -0,0 +1,25 @@
+﻿using Shopping.Aggregator.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddHttpClientServices(this IServiceCollection services, IConfiguration Configuration)
+        {
+            services.AddHttpClient<ICatalogService, CatalogService>(c =>
+                c.BaseAddress = new Uri(Configuration["ApiSettings:CatalogUrl"]));
+
+            services.AddHttpClient<IBasketService, BasketService>(c =>
+                c.BaseAddress = new Uri(Configuration["ApiSettings:BasketUrl"]));
+
+            services.AddHttpClient<IOrderService, OrderService>(c =>
+                c.BaseAddress = new Uri(Configuration["ApiSettings:OrderingUrl"]));
+            return services;
+        }
+    }
+}
